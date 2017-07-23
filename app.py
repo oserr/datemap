9e95627faa84b11dea5c91400bb43c1c2e6eb6b8
Omar Serrano <oser520@gmail.com>
2017-07-22 23:29:11 -0700

    Create base flask app to allow google MAPS API key to be inserted dynamically

---
+++ app.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python
+# app.py
+from flask import Flask
+from flask import render_template
+from flask import send_from_directory
+
+# Create the Flask application
+app = flask.Flask(__name__, static_url_path='')
+
+API_KEY = open('google-maps-api-key.txt').read()
+
+@app.route('/')
+def index():
+    '''Allows a user to get all the items.'''
+    return flask.render_template('index.html', key=API_KEY)
+
+if __name__ == '__main__':
+    app.debug = True
+    app.run(host='0.0.0.0', port=5000)

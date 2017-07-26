89a763ed233e6f4f4e871f05a1f8da2e121729c0
Omar Serrano <oser520@gmail.com>
2017-07-25 20:30:10 -0700

    Add docstring on centerModal()

--- static/app.js
+++ static/app.js
@@ -189,6 +189,10 @@ function showModal(marker) {
     );
 }
 
+/**
+ * Centers a modal div on a window.
+ * @param {jQuery object} modalDiv - The div representing the modal window.
+ */
 function centerModal(modalDiv) {
     const top = Math.max(modalDiv.height() - modalDiv.outerHeight(), 0) / 2;
     const left = Math.max(modalDiv.width() - modalDiv.outerWidth(), 0) / 2;

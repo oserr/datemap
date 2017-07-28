d9ac46aa8edb6021613403fecf05b283f780ef13
Omar Serrano <oser520@gmail.com>
2017-07-28 11:53:35 -0700

    Update app.py
    - import sys to use sys.stderr and sys.exit
    - fetch foursquare api id and secret
    - report errors for missing keys

--- app.py
+++ app.py
@@ -1,16 +1,56 @@
 #!/usr/bin/env python
 # app.py
 import flask
+import sys
+
+
+def app_err(msg):
+    '''Prints an error message to sys.stderr and terminates the program.'''
+    print('Error: %s' % msg , file=sys.stderr)
+    sys.exit(1)
+
+
+# Fetch the foursquare API client ID and secret
+fs_dict = {}
+fs = open('foursquare-api.txt').readlines()
+for line in fs:
+    if not line:
+        # Ignore blank lines
+        continue
+    key_value = line.split('=')
+    if len(key_value) != 2:
+        app_err('line does not contain key=value pair')
+    k, v = key_value
+    if not k:
+        app_err('Line does not contain key, but expecting key=value pairs')
+    if not v:
+        app_err('Line does not contain key value, but expecting key=value pairs')
+    fs_dict[k] = v
+
+client_id = fs_dict.get('client_id')
+if not client_id:
+    app_err('Unable to find client_id')
+
+client_secret = fs_dict.get('client_secret')
+if not client_secret:
+    app_err('Unable to find client_secret')
+
+# Fetch the Google Maps API key
+api_key = open('google-maps-api-key.txt').read()
+if not api_key:
+    app_err('Unable to find Google Maps API key')
 
 # Create the Flask application
 app = flask.Flask(__name__, template_folder='.')
 
-API_KEY = open('google-maps-api-key.txt').read()
-
 @app.route('/')
 def index():
     '''Allows a user to get all the items.'''
-    return flask.render_template('index.html', key=API_KEY)
+    return flask.render_template(
+        'index.html',
+        key=api_key,
+        client_id=client_id,
+        client_secret=client_secret)
 
 if __name__ == '__main__':
     app.debug = True
